41aec03b7303d5e98eab915031d3425b04e1ee3a
Nicoxso <nicolas-solartem@unilibre.edu.co>
2026-09-14 19:46:44 -0500

    Update Empresa.java

--- Empresa.java
+++ Empresa.java
@@ -13,14 +13,16 @@ public class Empresa {
     public static void main(String[] args) {
         Empleado domiciliario = new Domiciliario( "Carlos", 10065461, 1500000, "8:00 AM", 40, 10000 );
         Empleado practicante = new Practicante( "Laura", 1002, 800000,null );
+        Empleado gerente = new Gerente( "Miguel", 4353454, 2000000, "7:00 AM", 1200000);
         Proveedor proveedor = new Proveedor( "Supermercado asfdfsd", 900123456, 2500000 );
         
         System.out.println("//HORAS DE INGRESO//");
+        gerente.registrarHoraIngreso();
         domiciliario.registrarHoraIngreso();
         practicante.registrarHoraIngreso();
         
         System.out.println("//PAGOS DE EMPLEADOS//");
-        Empleado[] empleados = {domiciliario, practicante}; 
+        Empleado[] empleados = {domiciliario, practicante, gerente}; 
         for (Empleado empleado : empleados) { 
             System.out.println( empleado.getNombre() + " - Pago mensual: $" + empleado.pagoMensual() );
         }
